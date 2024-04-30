11b72f007c07702c08b3ce44ab355b96221b2f90
Matthew Chappee <mchappeespam@me.com>
2024-04-30 16:03:19 -0500

    Inital

---
+++ classes.php
@@ -0,0 +1,16 @@
+<?php
+
+  class Player {
+    public $name;
+    public $id;
+    public $consumearray;
+  }
+
+  class Consume {
+    public $consumename;
+    public $start;
+    public $end;
+  }
+
+?>
+
